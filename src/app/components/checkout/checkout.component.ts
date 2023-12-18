0ae2f23fbc649b6818a7d307d1746f11e72eb59a
Minh Chien DUONG <chienduong90@gmail.com>
2023-12-18 22:32:43 +0100

    add checkout

---
+++ src/app/components/checkout/checkout.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-checkout',
+  templateUrl: './checkout.component.html',
+  styleUrls: ['./checkout.component.css']
+})
+export class CheckoutComponent implements OnInit {
+
+  checkoutFormGroup = this.formBuilder.group({
+    customer: this.formBuilder.group({
+      firstName: [''],
+      lastName: [''],
+      email: ['']
+    }),
+    shippingAddress: this.formBuilder.group({
+      street: [''],
+      city: [''],
+      state: [''],
+      country: [''],
+      zipCode: ['']
+    }),
+    billingAddress: this.formBuilder.group({
+      street: [''],
+      city: [''],
+      state: [''],
+      country: [''],
+      zipCode: ['']
+    }),
+    creditCard: this.formBuilder.group({
+      cardType: [''],
+      nameOnCard: [''],
+      cardNumber: [''],
+      securityCode: [''],
+      expirationMonth: [''],
+      expirationYear: ['']
+    })
+  });
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit(): void { }
+
+  onSubmit() {
+    console.log("Handling the submit button");
+    console.log(this.checkoutFormGroup.get('customer')?.value);
+    console.log(this.checkoutFormGroup.get('customer')?.value.email);
+  }
+
+}
