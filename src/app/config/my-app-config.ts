a7dc1342abd35879ecf7fca1ebca9eea54365fce
Minh Chien DUONG <chienduong90@gmail.com>
2023-12-22 14:49:31 +0100

    security communication with https and TSL

--- src/app/config/my-app-config.ts
+++ src/app/config/my-app-config.ts
@@ -3,7 +3,7 @@ export default {
   oidc: {
     clientId: '0oae1jnvyxvb93cO45d7',
     issuer: 'https://dev-72336097.okta.com/oauth2/default',
-    redirectUri: 'http://localhost:4200/login/callback',
+    redirectUri: 'https://localhost:4200/login/callback',
     scopes: ['openid', 'profile', 'email']
   }
 }
